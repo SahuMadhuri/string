87c8773bc7a68e2ed7f3f3c142b6cce8f5125006
SahuMadhuri <madhurisahu410@gmail.com>
2021-01-14 15:48:59 +0530

    initial value

---
+++ Concatenate.java
@@ -0,0 +1,11 @@
+package in.basic.String;
+
+public class Concatenate {
+	public static void main(String[] args) {
+		String s="Vijay";
+		String s1="Chauhan";
+		System.out.println(s+" "+s1);
+				
+	}
+
+}
